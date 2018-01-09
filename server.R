643fded6de69cceed89283bdd5e9d8fac44bddec
ck2136 <chong.kim@ucdenver.edu>
2018-01-08 22:27:22 -0700

    'First Commit"

---
+++ server.R
@@ -0,0 +1,469 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(survival)
+library(splines)
+library(MASS)
+library(flexsurv)
+library(dplyr)
+
+library(formattable)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  
+  #This function is repsonsible for loading in the selected file
+  
+  filedata1 <- reactive({
+    infile <- input$datafile1
+    if (is.null(infile)) {
+      # User has not uploaded a file yet
+      return(NULL)
+    }
+    read.csv(infile$datapath)[1:max(na.omit(read.csv(infile$datapath))),]
+
+  })
+  
+  filedata2 <- reactive({
+    infile <- input$datafile2
+    if (is.null(infile)) {
+      # User has not uploaded a file yet
+      return(NULL)
+    }
+    read.csv(infile$datapath)[complete.cases(read.csv(infile$datapath)),]
+    
+  })
+  
+  # This allows user to select variables in the filedata
+  # First variable will be the survival probability variable (Column 5 in the excel sheet)
+  output$Variables1 <- renderUI({
+    selectInput('vars1', 'S(t): Select the variable/column that represents the survival probabilities', names(filedata1()) , multiple = FALSE)
+  })
+  
+  
+  # Second variable will be the time variable (Column 2 in the excel sheet)
+  output$Variables2 <- renderUI({
+    selectInput('vars2', 'Time: Select the variable/column that represents the time', names(filedata1()) , multiple = FALSE)
+  })
+  
+  #This previews the CSV data files
+  output$filetable1 <- renderDataTable({
+    filedata1()
+  })
+  
+  output$filetable2 <- renderDataTable({
+    filedata2()
+  })
+  
+  # R session info
+  output$info <- renderPrint({
+    sessionInfo()
+  })
+  
+  output$dat1 <- renderPrint({
+    IPD()
+  })
+  
+  output$exp <- renderPrint({
+    expfl()
+  })
+  
+  # Guyot algorithm for recreating survival curve
+  
+  
+  #Output IPD
+  IPD <- reactive({
+    #Read in survival times 
+    t.S<-filedata1()[,input$vars2]
+    S<-filedata1()[,input$vars1]
+    
+    #Read in published numbers at risk, n.risk, at time, t.risk, lower and upper
+    # indexes for time interval
+    t.risk<-filedata2()[,2]
+    lower<-filedata2()[,3]
+    upper<-filedata2()[,4]
+    n.risk<-filedata2()[,5]
+    n.int<-length(n.risk)
+    n.t<- upper[n.int]
+    
+    #Initialise vectors
+    arm<-rep(input$arm.id,n.risk[1]) 
+    n.censor<- rep(0,(n.int-1))
+    n.hat<-rep(n.risk[1]+1,n.t)
+    cen<-rep(0,n.t)
+    d<-rep(0,n.t)
+    KM.hat<-rep(1,n.t)
+    last.i<-rep(1,n.int)
+    sumdL<-0
+    
+    
+    try(
+      if (n.int > 1){
+        #Time intervals 1,...,(n.int-1)
+        for (i in 1:(n.int-1)){
+          #First approximation of no. censored on interval i
+          n.censor[i]<- round(n.risk[i]*S[lower[i+1]]/S[lower[i]]- n.risk[i+1])
+          #Adjust tot. no. censored until n.hat = n.risk at start of interval (i+1)
+          while((n.hat[lower[i+1]]>n.risk[i+1])||((n.hat[lower[i+1]]<n.risk[i+1])&&(n.censor[i]>0))){
+            if (n.censor[i]<=0){
+              cen[lower[i]:upper[i]]<-0
+              n.censor[i]<-0
+            }
+            if (n.censor[i]>0){
+              cen.t<-rep(0,n.censor[i])
+              for (j in 1:n.censor[i]){
+                cen.t[j]<- t.S[lower[i]] +
+                  j*(t.S[lower[(i+1)]]-t.S[lower[i]])/(n.censor[i]+1)
+              }
+              #Distribute censored observations evenly over time. Find no. censored on each time interval.
+              cen[lower[i]:upper[i]]<-hist(cen.t,breaks=t.S[lower[i]:lower[(i+1)]],
+                                           plot=F)$counts 
+            }
+            #Find no. events and no. at risk on each interval to agree with K-M estimates read from curves
+            n.hat[lower[i]]<-n.risk[i]
+            last<-last.i[i] 
+            for (k in lower[i]:upper[i]){
+              if (i==1 & k==lower[i]){
+                d[k]<-0
+                KM.hat[k]<-1
+              }
+              else {
+                d[k]<-round(n.hat[k]*(1-(S[k]/KM.hat[last])))
+                KM.hat[k]<-KM.hat[last]*(1-(d[k]/n.hat[k]))
+              }
+              n.hat[k+1]<-n.hat[k]-d[k]-cen[k]
+              if (d[k] != 0) last<-k
+            }
+            n.censor[i]<- n.censor[i]+(n.hat[lower[i+1]]-n.risk[i+1])
+          }
+          if (n.hat[lower[i+1]]<n.risk[i+1]) n.risk[i+1]<-n.hat[lower[i+1]]
+          last.i[(i+1)]<-last
+        }
+      }
+      ,
+      silent=TRUE
+    )
+    
+    
+    #Time interval n.int.
+    try(
+      {if (n.int>1){
+        #Assume same censor rate as average over previous time intervals.
+        n.censor[n.int]<- min(round(sum(n.censor[1:(n.int-1)])*(t.S[upper[n.int]]-
+                                                                  t.S[lower[n.int]])/(t.S[upper[(n.int-1)]]-t.S[lower[1]])), n.risk[n.int])
+      }
+        
+        
+        
+        if (n.int==1){n.censor[n.int]<-0}
+        if (n.censor[n.int] <= 0){
+          cen[lower[n.int]:(upper[n.int]-1)]<-0
+          n.censor[n.int]<-0
+          
+          if (n.censor[n.int]>0){
+            cen.t<-rep(0,n.censor[n.int])
+            for (j in 1:n.censor[n.int]){
+              cen.t[j]<- t.S[lower[n.int]] +
+                j*(t.S[upper[n.int]]-t.S[lower[n.int]])/(n.censor[n.int]+1)
+            }
+            cen[lower[n.int]:(upper[n.int]-1)]<-hist(cen.t,breaks=t.S[lower[n.int]:upper[n.int]],
+                                                     plot=F)$counts 
+          }
+        }
+        #Find no. events and no. at risk on each interval to agree with K-M estimates read from curves
+        n.hat[lower[n.int]]<-n.risk[n.int]
+        last<-last.i[n.int]
+        for (k in lower[n.int]:upper[n.int]){
+          if(KM.hat[last] !=0){
+            d[k]<-round(n.hat[k]*(1-(S[k]/KM.hat[last])))} else {d[k]<-0}
+          KM.hat[k]<-KM.hat[last]*(1-(d[k]/n.hat[k]))
+          n.hat[k+1]<-n.hat[k]-d[k]-cen[k]
+          #No. at risk cannot be negative
+          if (n.hat[k+1] < 0) {
+            n.hat[k+1]<-0
+            cen[k]<-n.hat[k] - d[k]
+          }
+          if (d[k] != 0) last<-k
+        }
+        
+        
+        #If total no. of events reported, adjust no. censored so that total no. of events agrees.
+        if (tot.events != "NA"){
+          if (n.int>1){
+            sumdL<-sum(d[1:upper[(n.int-1)]])
+            #If total no. events already too big, then set events and censoring = 0 on all further time intervals
+            if (sumdL >= tot.events){
+              d[lower[n.int]:upper[n.int]]<- rep(0,(upper[n.int]-lower[n.int]+1))
+              cen[lower[n.int]:(upper[n.int]-1)]<- rep(0,(upper[n.int]-lower[n.int]))
+              n.hat[(lower[n.int]+1):(upper[n.int]+1)]<- rep(n.risk[n.int],(upper[n.int]+1-lower[n.int]))
+            }
+          }
+          #Otherwise adjust no. censored to give correct total no. events
+          if ((sumdL < tot.events)|| (n.int==1)){
+            sumd<-sum(d[1:upper[n.int]])
+            while ((sumd > tot.events)||((sumd< tot.events)&&(n.censor[n.int]>0))){
+              n.censor[n.int]<- n.censor[n.int] + (sumd - tot.events)
+              if (n.censor[n.int]<=0){
+                cen[lower[n.int]:(upper[n.int]-1)]<-0
+                n.censor[n.int]<-0
+              }
+              if (n.censor[n.int]>0){
+                cen.t<-rep(0,n.censor[n.int])
+                for (j in 1:n.censor[n.int]){
+                  cen.t[j]<- t.S[lower[n.int]] +
+                    j*(t.S[upper[n.int]]-t.S[lower[n.int]])/(n.censor[n.int]+1)
+                }
+                cen[lower[n.int]:(upper[n.int]-1)]<-hist(cen.t,breaks=t.S[lower[n.int]:upper[n.int]],
+                                                         plot=F)$counts 
+              }
+              n.hat[lower[n.int]]<-n.risk[n.int]
+              last<-last.i[n.int]
+              for (k in lower[n.int]:upper[n.int]){
+                d[k]<-round(n.hat[k]*(1-(S[k]/KM.hat[last])))
+                KM.hat[k]<-KM.hat[last]*(1-(d[k]/n.hat[k]))
+                if (k != upper[n.int]){
+                  n.hat[k+1]<-n.hat[k]-d[k]-cen[k]
+                  #No. at risk cannot be negative
+                  if (n.hat[k+1] < 0) {
+                    n.hat[k+1]<-0
+                    cen[k]<-n.hat[k] - d[k]
+                  }
+                }
+                if (d[k] != 0) last<-k
+              }
+              sumd<- sum(d[1:upper[n.int]])
+            }
+          }
+        }
+        
+      },
+      silent = TRUE
+    )
+    
+    #Initialise vectors
+    try({
+      t.IPD<-rep(t.S[n.t],n.risk[1])
+      event.IPD<-rep(0,n.risk[1])
+      #Write event time and event indicator (=1) for each event, as separate row in t.IPD and event.IPD
+      k=1
+      for (j in 1:n.t){
+        if(d[j]!=0){
+          t.IPD[k:(k+d[j]-1)]<- rep(t.S[j],d[j])
+          event.IPD[k:(k+d[j]-1)]<- rep(1,d[j])
+          k<-k+d[j]
+        }
+      }
+    },
+    silent = TRUE)
+    
+    #Write censor time and event indicator (=0) for each censor, as separate row in t.IPD and event.IPD
+    for (j in 1:(n.t-1)){
+      if(cen[j]!=0){
+        t.IPD[k:(k+cen[j]-1)]<- rep(((t.S[j]+t.S[j+1])/2),cen[j])
+        event.IPD[k:(k+cen[j]-1)]<- rep(0,cen[j])
+        k<-k+cen[j]
+      }
+    }
+    
+    #Output IPD
+    
+    IPData <-matrix(c(t.IPD,event.IPD,arm),ncol=3,byrow=F)
+    IPData <-as.data.frame(IPData)
+    #IPData
+    IPData
+  })
+  
+  KMest <- reactive({
+    survfit(Surv(IPD()[,1],IPD()[,2])~1,data=IPD(),type="kaplan-meier")
+  })
+
+  output$KMsum <- renderPrint({
+    summary(KMest())
+  })
+
+  
+  # Fit survival distributions
+  
+  expfl <- reactive({
+    flexsurvreg(Surv(IPD()[,1], IPD()[,2])~1, dist="exponential")   # Exponential function, interval censoring				
+  })
+  
+  gengam <- reactive({
+    flexsurvreg(Surv(IPD()[,1], IPD()[,2])~1, dist="gengamma")   # Generalized gamma function, interval censoring		
+  }) 										
+
+  gompert <- reactive({
+    flexsurvreg(Surv(IPD()[,1], IPD()[,2])~1, dist="gompertz")   # Generalized gamma function, interval censoring	
+  }) 											
+
+  llogfl <- reactive({
+    flexsurvreg(Surv(IPD()[,1], IPD()[,2])~1, dist="llogis")   # Loglogistic function, interval censoring	
+  }) 											
+
+  lnormfl <- reactive({
+    flexsurvreg(Surv(IPD()[,1], IPD()[,2])~1, dist="lognormal") 
+  }) 
+  
+  weibfl <- reactive({
+    flexsurvreg(Surv(IPD()[,1], IPD()[,2])~1, dist="weibull")   # Loglogistic function, interval censoring						
+  })
+  
+  # Make Plots
+  
+  makeplot <- function(){
+    # If splin == yes, include splin in the distribution plot
+    if(input$splin == 'yes'){
+      
+      SurvObj <- with(IPD(), Surv(IPD()[,1], IPD()[,2]))
+      KM <- survfit(SurvObj ~ 1, data=IPD())
+      
+      plot(KM, xmax=45, xlab = "months", ylab = "S(t)", main = "Survival Plot")
+      # add the other distribution lines 
+      #Plot Exponential
+      lines(expfl(), col="red", ci= ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      #Plot gen gamma
+      lines(gengam(), col="orange", ci= ifelse(input$confint == 'yes', TRUE, FALSE))
+      #Plot log-logistic
+      lines(llogfl(), col="yellow", ci= ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      lines(weibfl(), col="green", ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      
+      lines(lnormfl(), col = 'blue', ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      lines(gompert(), color = "purple", ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      lines(spline1, color = "chocolate", ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      ## Add legends
+      legend(x = "bottomleft",
+             legend = c("Kaplan-Meier","Exp" ,"Gen-Gamma", "Log-logistic", "Weibull", "Log-Normal", "Gompertz", "Spline"),
+             lwd = 2, bty = "n", lty=c(1,1,1,1,1,1,1,1),
+             col = c("black", "red","orange", "yellow", "green", "blue", "purple", "chocolate"))
+      
+    } else {
+      # no spline plot
+      SurvObj <- with(IPD(), Surv(IPD()[,1], IPD()[,2]))
+      KM <- survfit(SurvObj ~ 1, data=IPD())
+      
+      plot(KM, xmax=45, xlab = "months", ylab = "S(t)", main = "Survival Plot")
+      # add the other distribution lines 
+      #Plot Exponential
+      lines(expfl(), col="red", ci= ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      #Plot gen gamma
+      lines(gengam(), col="orange", ci= ifelse(input$confint == 'yes', TRUE, FALSE))
+      #Plot log-logistic
+      lines(llogfl(), col="yellow", ci= ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      lines(weibfl(), col="green", ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      
+      lines(lnormfl(), col = 'blue', ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      lines(gompert(), color = "purple", ci = ifelse(input$confint == 'yes', TRUE, FALSE))
+      
+      ## Add legends
+      legend(x = "bottomleft",
+             legend = c("Kaplan-Meier","Exp" ,"Gen-Gamma", "Log-logistic", "Weibull", "Log-Normal", "Gompertz"),
+             lwd = 2, bty = "n", lty=c(1,1,1,1,1,1,1),
+             col = c("black", "red","orange", "yellow", "green", "blue", "purple"))
+      
+    } 
+  }
+  
+  output$survplot <- renderPlot({
+    print(makeplot())
+  })
+  
+  
+  # Tabular Numbers
+  
+  ## Standardized Solutions
+  
+  output$stdsol <- renderFormattable({
+    formattable(standardizedsolution(est()$fit),  digits = 4)
+  })
+
+  ## Overall Summary of Model
+  
+  result <- reactive({
+    if(input$anal == 'pls'){
+      res <- plsmod()
+      res <- summary(res)
+    } else {
+      res <- est()$fit
+      res <- summary(res, standardized=TRUE, fit.measures=TRUE)
+    } 
+    res
+  })
+  result1 <- reactive({
+    if(input$anal == 'sem' || input$anal == 'growth' || input$anal == 'cfa'){
+      res <- est()$fit
+      res <- summary(res, standardized=TRUE, fit.measures=TRUE)
+    }
+    res
+  })
+  
+  result2 <- reactive({
+    if(input$anal == 'efa'){
+      res <- est()
+    }
+    res
+  })
+  
+  resultpca <- reactive({
+    if(input$anal == 'efa' && input$efalysisopt == 'pca'){
+      res <- est()
+    }
+    res$fit$loadings
+  })
+  
+  output$result <- renderPrint({
+    result()
+  })
+  output$result1 <- renderPrint({
+    result1()
+  })
+  output$resultefa <- renderPrint({
+    result2()
+  })
+  output$resultefapca <- renderPrint({
+    resultpca()
+  })
+  
+  outputOptions(output, 'result', suspendWhenHidden=FALSE)
+  # Rsquared
+  
+  output$aicbic <- renderPrint({
+    if(input$anal == 'pls'){
+      r2 <- plsmod()$inner_summary[, "R2", drop = FALSE]
+    } else {
+      r2 <- inspect(est()$fit, "rsquare")
+    }
+    r2
+  })
+  
+  # Fit measures
+  
+  output$fm <- renderPrint({
+    if(input$anal == 'pls'){
+      fm <- plsmod()$gof
+    } else {
+      fm <- fitmeasures(est()$fit)
+    }
+    fm
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,198 @@
+### User interface for the Survival Curve Extraction Dashboard ###
+
+## ui.R ##
+library(shinydashboard)
+library(DT)
+library(shinyAce)
+library(formattable)
+dashboardPage(
+  
+  # Header
+  dashboardHeader(title = "Survival Curve Extraction Dashboard"),
+  
+  
+  # Sidebar Menus
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Introduction", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Data Input", tabName = "data", icon = icon("th")),
+      menuItem('Curve Extraction', tabName = 'results', icon = icon('th'), 
+                 collapsible = 
+                   menuSubItem('random', tabName = 'random'),
+                   menuSubItem('Plots', tabName = 'plot'),
+                   menuSubItem('Parameter Estimates', tabName = 'regresult'),
+                   menuSubItem('Fit Statistics', tabName = 'diag')
+        )
+      
+      
+    )
+  ),
+  
+  # Body of the Dashboard
+  dashboardBody(
+    tabItems(
+      
+      # First tab content DashBoard
+      tabItem(tabName = "dashboard",
+              
+              h2("Introduction"),
+              fluidRow(
+                box(
+                  h4('This is a dashboard for generating survival curve distribution parameters, fit statistic, and figures'),
+                  h4("There are several important key points to remember"),
+                  h4(strong("Number 1:"),"User needs to have 2 .csv files to input which is described in Guyot et al. 2012: Enhanced secondary analysis of survival data: reconstructing the data from published Kaplan-Meier survival curves"),
+                  h4(strong("If Number 1 is not satisfied:"),"User needs to extract the survival curve data points by using a figure digitizer such as Webplotdigitizer (e.g. https://apps.automeris.io/wpd/) and extract the curve data and relevant risk and censoring information as described in Guyot et al."),
+                  h4(strong("Number 2:"),"Before trying to determine a survival distribution that best fits the data, user needs to check that the extracted curve is as accurate as possible. Human error is highly possible to be introduced during the process of survival curve extraction thus this step is crucial in terms of preserving the integrity of the original study survival information"),
+                  h4(strong("Number 3:"),"The outputs that are generated in the 'Result' tab (Bottom tab on the left) needs the user to be comfortable with the 'survival' package output. Additional understanding of packages such as 'flexsurv' and 'spline' will be useful as additional distributions may be specified using those packages"),
+                  h4(strong("Below:"),"are some examples of the steps outlined above. If you have any questions regarding how to go about each step please consult Guyot et al. first and if still difficult to proceed e-mail: chong.kim@ucdenver.edu")  
+                  
+                ,width = 12)
+              ),
+              
+              fluidRow(
+                box(
+                  h4(strong("Number 1")),
+                  
+                  img(src='step1.gif', align = 'center')
+                  , width = 12 , align = 'center'
+                )
+                
+              ),
+              
+              fluidRow(
+                column(8,
+                  box(
+                    h4(strong("Number 2")),
+                    
+                    img(src='step2.gif', align = 'center'),
+                    
+                    h4("Currently the most important aspect with regards to checking whether or not the survival curve is appropriate is to determine if it is monotone decreasing (i.e. not increasing). Due to approximation errors in the webplotdigitizer, there may be points that have higher S(t) than the previous time point. ")
+                    
+                    
+                    , width = 8, align = 'center'
+                  ), offset = 3
+                )
+                
+                
+              )
+              
+              
+      ),
+      
+      # Second Tab content: Data and SUmmary
+      tabItem(tabName = "data",
+              
+              h2("Please Enter the CSV Data"),
+              
+              fileInput('datafile1', 'Please Input the Survival Curve CSV file',
+                        accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+              
+              
+              h3("Select the Appropriate Variables for Parameter Estimation"),
+              uiOutput("Variables1"),
+              
+              uiOutput("Variables2"),
+              
+              fileInput('datafile2', 'Please Input the Number at Risk CSV file',
+                        accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+              
+              h3("Enter total number of events"),
+              numericInput('totevent',"Total Number of Events",value =  NA, min = 0),
+              
+              h3("Control or Treatment Indicator. Control = 0; Treatment = 1"),
+              numericInput('arm.id',"Arm ID", 1, min = 0, max = 1),
+              
+              # Options to include splines and how many knots and etc
+              
+              fluidRow(
+                box(
+                  radioButtons('splin','Include Splines?',
+                              choices =  c(
+                                "Yes" = 'yes',
+                                "No" = 'no'), selected = 'no')
+                )),
+              
+              
+              # View quick summary statistics
+              # Create row to check the data
+              h3("Data View of Curve Data"),
+              fluidRow( dataTableOutput("filetable1")),
+              
+              h3("Data View of Number at Risk Data"),
+              fluidRow( dataTableOutput("filetable2")),
+              
+              h3("Raw Individual Patient Data (IPD) View"),
+              verbatimTextOutput("dat1"),
+              
+              h3("Survival Probability Table"),
+              verbatimTextOutput("KMsum"),
+              
+              strong('R session info'),
+              verbatimTextOutput("info")
+              
+      ),
+
+    
+      # Parameter Estimates tab content
+      tabItem(tabName = "regresult",
+              
+              h2("Parameter Estimates"),
+              
+              fluidRow(
+                box(
+                  h3("Survival Distribution Parameters"),
+                  verbatimTextOutput("exp")
+                  ,width = 12
+                )
+              )
+              
+              
+      ),
+      
+      # Plots tab: Plot submenu of result
+      tabItem(tabName = "plot",
+              
+              fluidRow(
+                box(
+                  h3("Plotting Options"),
+                  radioButtons('confint','Confidence Intervals?',
+                               choices =  c(
+                                 "Yes" = 'yes',
+                                 "No" = 'no'), selected = 'no')
+                  ,width = 4
+                )
+              ),
+            
+              fluidRow(
+                box(width = 12,
+                    h2("Survival Distribution Plot"),
+                    fluidRow(
+                      box(
+                        plotOutput("survplot", width = 700)
+                      )
+                    )
+              )
+    )
+      ),
+      
+      # Sixth tab: Fit statistics/Diagnostic
+      tabItem(tabName = "diag",
+              h2("Fit Statistics"),
+              
+              fluidRow(
+                box(
+                  h4("AIC and BIC"),
+                  verbatimTextOutput("aicbic")
+                  , width = 12
+                )
+              )
+              
+              
+      )
+      
+      
+      
+      
+    )
+  )
+)
